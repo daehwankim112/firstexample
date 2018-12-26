c7e730812927d41e27a160119d97e8e5fc6696dd
daehwankim112 <kdw8731@gmail.com>
2018-12-26 15:28:31 -0400

    original changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
-
+    //The original
     public static void main(String[] args) {
-
         Scanner sc = new Scanner( System.in );
-        System.out.println("sc.next()");
+        System.out.println(sc.next());
     }
 }
