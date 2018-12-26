484d088087fb374345d64222a65ef12335bc7e54
daehwankim112 <kdw8731@gmail.com>
2018-12-26 16:24:20 -0400

    out of date but let's try

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    //The original
+    //This project is out of date
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
         System.out.println(sc.next());
