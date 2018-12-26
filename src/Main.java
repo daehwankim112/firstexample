4de4ecf50e2f5595ec350fb4c7417a1dd8e6ba97
daehwankim112 <kdw8731@gmail.com>
2018-12-26 16:14:14 -0400

    new changes from conflict directory

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    //The original
+    //Another change please work this time
     public static void main(String[] args) {
         Scanner sc = new Scanner( System.in );
         System.out.println(sc.next());
